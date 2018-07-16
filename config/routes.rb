86c5efbc21da7a48406cd7b3cf5c03d13d60f493
John <bernard.baclay@go-va.com.au>
2018-07-16 19:56:19 +0800

    Initial setup for the login

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
