b06c0780926c1b5d04b7b9a628c1da574312c394
John <bernard.baclay@go-va.com.au>
2018-07-17 19:40:47 +0800

    Added jquery-ujs to application.js  and rails_12factor to gemfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     respond_to do |format|
       if @user.save
-        format.html { redirect_to articles_path, notice: 'User was successfully created.' }
+        session[:user_id] = @user.id
+        format.html { redirect_to user_path(@user), notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
 
 
