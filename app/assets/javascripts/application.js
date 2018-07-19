d32a6b9a7720780ab25087a6f710df3dcfba7d74
John <bernard.baclay@go-va.com.au>
2018-07-19 17:49:18 +0800

    Removed rails-ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+
+//= require jquery
+//= requre jquery-ujs
 //= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
